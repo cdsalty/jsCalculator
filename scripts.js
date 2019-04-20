b42e48c90b4f7cb470a9c93f3763178a9e0f8ff5
cdsalty <soltiscd@gmail.com>
2019-04-20 14:46:58 -0400

    removed functions

--- scripts.js
+++ scripts.js
@@ -2,28 +2,11 @@
 
 //Step 2: Create a Class (and a constructor function;)
 class calculator {
-    constructor(previousInput, currentInput) { //previousInput and currentInput will direct where to display our display text for the calculator)
-        //Inside this function, create variables for the class.
-        this.previousInput = previousInput;
+    constructor(previousInput, currentInput) { 
+        this.previousInput = previousInput; // gives ability to set these elements inside our calculator class.   
         this.currentInput = currentInput;
     }
-    //Consider each operation the calculator will need to perform and create a function for each one. 
-
-    clear() {
-
-    }
-
-    delete(){
-
-    }
-
-    appendNumber(){ // what will happeh every time a user clicks a number to add to the screen. 
-
-    }
-
-    chooseOperation(operation){ // will happen when a user clicks on one of our four operation buttons. The funciton must take-in the operator the user selected;
-
-    }
+     
 }
 
 
