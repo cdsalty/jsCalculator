93f17704a6b8374b1a3fee0eb7f28b39db33fda8
cdsalty <soltiscd@gmail.com>
2019-04-23 12:19:50 -0400

    displaying next to compute

--- scripts.js
+++ scripts.js
@@ -10,8 +10,8 @@ class Calculator {
     }
     //Step 3: Consider all the operations our calculator class can perform
     clear(){ // To clear out our different variables; what specifically to clear out.
-        this.currentData = '';  // the input being entered on the lower portion of the calculator display
-        this.previousData = ''; // the input previously entered, stored/display on the top of the calculator display
+        this.currentOperation = '';  // the input being entered on the lower portion of the calculator display
+        this.previousOperation = ''; // the input previously entered, stored/display on the top of the calculator display
         this.operation = undefined; // if we clear things, there will be no current, previous or operations displayed; undefined;
         // this is setting the properties displayed in the calculator window to empty strings; call it as soon as a new instance of
         // the calculator is created.
@@ -22,13 +22,21 @@ class Calculator {
     }
 
     appendNumber(number){ // What will happen each time a user clicks a number; We will need to pass the number param the user selected.
-        // this.currentData = number; // this only appends one number; will not allow mulitple numbers.
-        this.currentData = this.currentData.toString() + number.toString(); // Otherwise, 1 + 1 would be 2 vs. 1 & 1 showing 11
-
+        // this.currentOperation = number; // this only appends one number; will not allow mulitple numbers.
+        if (number === '.' && this.currentOperation.includes('.'))   // prevents multiple decimals being typed. If no decimal in place, it will not run
+        return;
+        this.currentOperation = this.currentOperation.toString() + number.toString(); // Otherwise, 1 + 1 would be 2 vs. 1 & 1 showing 11
     }
 
-    chooseOperation(operation){  // anytime a user clicks on the operation. This function will need to take the operation selected.
-
+    chooseOperation(operation){  // anytime a user clicks on the operation; this function will take the operation selected and do something.
+        if (this.currentOperation === '') 
+        return;
+        if (this.previousOperation !== ''){
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperation = this.currentOperation; // making the previousOperation now hold the currentOperation that was input
+        this.currentOperation = ''; // now the currentOperation position is empty. 
     }
 
     compute(){  // will take values inside our calculator and compute a singe value for what needs to display.
@@ -36,7 +44,7 @@ class Calculator {
     }
 
     updateDisplay(){    // this will update the values inside of our output.
-        this.currentInputElement.innerText=this.currentData;
+        this.currentInputElement.innerText=this.currentOperation;
     }
      
 }
@@ -45,7 +53,7 @@ class Calculator {
 
 //Step 1: Assign buttons to variables (To Call on them later)
 const numberBtns = document.querySelectorAll('[data-number]');
-const operationBtn = document.querySelectorAll('[data-operator]');
+const operationBtns = document.querySelectorAll('[data-operator]');
 const equalsBtn = document.querySelector('[data-equals]')
 const deleteBtn = document.querySelector('[data-delete]');
 const clearScreen = document.querySelector('[data-allclear]');
@@ -58,9 +66,20 @@ const calculator = new Calculator(previousInputElement, currentInputElement);
 // syntax: var object = new Object();
 
 // Step 5: Add EventListners 
+    // For our buttons that are numbers
 numberBtns.forEach((button) => {
     button.addEventListener('click', () => {
-        calculator.appendNumber(button.innerText);
+        //console.log("sanity check");
+        calculator.appendNumber(button.innerText);  // hey mr. appendNumber function in the calculator, I want you to update the 
+                                                    // display to show the innerText of the button clicked. 0-9 and .
+        calculator.updateDisplay();                 // updateDisplay, I want to call you too
+    })
+});
+
+    // For our operations button (Will make use of the chooseOperation function)
+operationBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
         calculator.updateDisplay();
     })
 });
