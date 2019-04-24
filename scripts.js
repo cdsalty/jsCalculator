a4880957c1c7cf81e46f4794d6bdc09aa8310c15
cdsalty <soltiscd@gmail.com>
2019-04-23 23:52:44 -0400

    fixed small bug in js

--- scripts.js
+++ scripts.js
@@ -2,15 +2,15 @@
 
 //Step 2: Create a Class (and a constructor function;)
 class Calculator {
-    constructor(previousInputElement, currentInputElement) { 
-        this.previousInputElement = previousInputElement; // gives ability to set these elements inside our calculator class.   
-        this.currentInputElement = currentInputElement;
+    constructor(previousOperandTextElement, currentOperandTextElement) { 
+        this.previousOperandTextElement = previousOperandTextElement; // gives ability to set these elements inside our calculator class.   
+        this.currentOperandTextElement = currentOperandTextElement;
         // As soon as we create our calculator, we want to call the 'clear function' to zero out everything; the default display
         this.clear(); // clears and sets to this default value.
     }
     //Step 3: Consider all the operations our calculator class can perform
     clear(){ // To clear out our different variables; what specifically to clear out.
-        this.currentOperation = '';  // the input being entered on the lower portion of the calculator display
+        this.currentOperand = '';  // the input being entered on the lower portion of the calculator display
         this.previousOperation = ''; // the input previously entered, stored/display on the top of the calculator display
         this.operation = undefined; // if we clear things, there will be no current, previous or operations displayed; undefined;
         // this is setting the properties displayed in the calculator window to empty strings; call it as soon as a new instance of
@@ -18,25 +18,23 @@ class Calculator {
     }
 
     delete(){ // to remove a single number
-
+        
     }
 
     appendNumber(number){ // What will happen each time a user clicks a number; We will need to pass the number param the user selected.
-        // this.currentOperation = number; // this only appends one number; will not allow mulitple numbers.
-        if (number === '.' && this.currentOperation.includes('.'))   // prevents multiple decimals being typed. If no decimal in place, it will not run
-        return;
-        this.currentOperation = this.currentOperation.toString() + number.toString(); // Otherwise, 1 + 1 would be 2 vs. 1 & 1 showing 11
+        // this.currentOperand = number; // this only appends one number; will not allow mulitple numbers.
+        if (number === '.' && this.currentOperand.includes('.')) return  // prevents multiple decimals being typed. If no decimal in place, it will not run
+        this.currentOperand = this.currentOperand.toString() + number.toString(); // Otherwise, 1 + 1 would be 2 vs. 1 & 1 showing 11
     }
 
     chooseOperation(operation){  // anytime a user clicks on the operation; this function will take the operation selected and do something.
-        if (this.currentOperation === '') 
-        return;
+        if (this.currentOperand === '') return
         if (this.previousOperation !== ''){
-            this.compute();
+            this.compute()
         }
         this.operation = operation;
-        this.previousOperation = this.currentOperation; // making the previousOperation now hold the currentOperation that was input
-        this.currentOperation = ''; // now the currentOperation position is empty. 
+        this.previousOperation = this.currentOperand; // making the previousOperation now hold the currentOperand that was input
+        this.currentOperand = ''; // now the currentOperand position is empty. 
     }
 
     compute(){  // will take values inside our calculator and compute a singe value for what needs to display.
@@ -44,7 +42,8 @@ class Calculator {
     }
 
     updateDisplay(){    // this will update the values inside of our output.
-        this.currentInputElement.innerText=this.currentOperation;
+        this.currentOperandTextElement.innerText=this.currentOperand;
+        this.previousOperandTextElement.innerText=this.previousOperation; //overlooked this function and what a nightmare 
     }
      
 }
@@ -57,11 +56,11 @@ const operationBtns = document.querySelectorAll('[data-operator]');
 const equalsBtn = document.querySelector('[data-equals]')
 const deleteBtn = document.querySelector('[data-delete]');
 const clearScreen = document.querySelector('[data-allclear]');
-const previousInputElement = document.querySelector('[data-previous-input]');
-const currentInputElement = document.querySelector('[data-current-input]');
+const previousOperandTextElement = document.querySelector('[data-previous-input]');
+const currentOperandTextElement = document.querySelector('[data-current-input]');
 
 // Step 4: Create a calculator object and pass everything into it.
-const calculator = new Calculator(previousInputElement, currentInputElement);
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
 // I originally had "new (c)alculator" the lowercase caused a "not defined statement." 
 // syntax: var object = new Object();
 
